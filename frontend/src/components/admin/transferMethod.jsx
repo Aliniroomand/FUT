1b0af3c203a1a4d68b8259da2e701586feade570
Aliniroomand <aliniroomand32@gmail.com>
2025-07-22 11:41:43 +0330

    (feat)admin-panel(frontend):organiation admin components and add card range frontend part 1(its not completed)

--- frontend/src/components/admin/transferMethod.jsx
+++ frontend/src/components/admin/transferMethod.jsx
@@ -4,7 +4,7 @@ import {
   createTransferMethod,
   updateTransferMethod,
   deleteTransferMethod,
-} from '../services/api';
+} from '../../services/api';
 
 const TransferMethods = () => {
   const [methods, setMethods] = useState([]);
--- frontend/src/pages/AdminDashbord.jsx
+++ frontend/src/pages/AdminDashbord.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import PriceManager from '../components/PriceManager';
-import TransferMethods from '../components/transferMethod';
+import TransferMethods from '../components/admin/transferMethod';
+import PriceManager from '../components/admin/PriceManager';
+import { CardRangeForm } from '../components/admin/CardRangeForm';
 
 const AdminDashboard = () => {
   return (
-    <div className="min-h-screen bg-black text-white p-8">
+    <div dir="rtl" className="min-h-screen bg-black text-white p-8 ">
       <h1 className="text-2xl font-bold mb-6 text-[#B8860B]">پنل ادمین</h1>
-      <PriceManager />
-      <TransferMethods />
+      {/* <PriceManager /> */}
+      {/* <TransferMethods /> */}
+      <CardRangeForm />
     </div>
   );
 };
